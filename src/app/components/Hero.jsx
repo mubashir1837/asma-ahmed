5af10c39d3068d86c5e2e24cf2667c864ff8342c
Mubashir Ali <mubashirali1837@gmail.com>
2025-02-08 20:03:51 +0500

    first commit

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,120 @@
+// components/Hero.jsx
+"use client"
+import React from 'react';
+import { FaWhatsapp, FaLinkedin, FaGithub, FaInstagram, FaFacebookSquare, FaYoutube, FaSkype, FaAward, FaCertificate, FaFolderOpen } from 'react-icons/fa';
+import Image from 'next/image';
+import { TypeAnimation } from "react-type-animation";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  return (
+    <div id='home' className=" bg-gradient-to-b from-yellow-200 to-orange-500  flex flex-col items-center justify-center  py-24 relative">
+      {/* Social Links */}
+      <div className="absolute left-2 top-[10%] lg:top-[60%] md:left-6 lg:left-8 transform md:-translate-y-1/2">
+        <ul className="space-y-4  p-2 rounded-full">
+          <li>
+            <a href="" className="text-green-600">
+              <FaWhatsapp size={30} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/asma-ahmed-8301a92a7/" className="text-[#0a66c2] hover:text-[#4b8ed1]">
+              <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/Asma-Ahmed-31" className="text-gray-900">
+              <FaGithub size={30}  />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/asma_ahmed_31/" className="text-pink-500 hover:text-pink-600">
+              <FaInstagram size={30}  />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com" className="text-blue-700 hover:text-blue-800">
+              <FaFacebookSquare size={30}  />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com" className="text-red-600 hover:text-red-700">
+              <FaYoutube size={30}  />
+            </a>
+          </li>
+          
+        </ul>
+      </div>
+      <div className="relative grid grid-cols-1 sm:grid-cols-12 h-full px-8 md:px-16 items-center ml-8">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mt-12 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-blue-700">
+              Hello, I&apos;m{" "}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                "Asma Ahmed",
+                1000,
+                "Bioinformatician",
+                1000,
+                "Data Analyst",
+                1000,
+                "Python Programmer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+              className='sm:text-xl lg:text-6xl'
+            />
+          </h1>
+          <p className="text-[#1d1e1f] text-base sm:text-sm mb-6 py-4 lg:text-xl">
+          A passionate teacher, creative graphic designer, and dedicated freelancer from Pakistan.
+          </p>
+          <div>
+            <Link
+              href="http://Wa.me/+923016408593"
+              className="px-6 inline-block py-3 font-bold w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#090d82] to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              href="https://heyzine.com/flip-book/a221b3d899.html"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#090d82] to-secondary-500  text-white mt-3"
+            >
+              <span className="block bg-gradient-to-br from-[#090d82] to-secondary-500 font-bold rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full bg-white  border-8  border-blue-900 shadow-2xl shadow-[#090d82] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative">
+            <Image
+              src="/asmaa.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full w-full h-full"
+              width={1000}
+              height={1000}
+            />
+          </div>
+        </motion.div>
+      </div>  
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,184 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { FaBriefcase } from "react-icons/fa";
+
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <div>
+      <nav className="bg-gray-900 z-10 fixed top-0 w-full">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <button
+                onClick={toggleMobileMenu}
+                type="button"
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 ring-purple- focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded={isMobileMenuOpen ? "true" : "false"}
+              >
+                <span className="sr-only">Open main menu</span>
+
+                <svg
+                  className={`${isMobileMenuOpen ? "hidden" : "block"} h-6 w-6`}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+
+                <svg
+                  className={`${isMobileMenuOpen ? "block" : "hidden"} h-6 w-6`}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex-shrink-0 items-center hidden lg:block text-white">
+                <FaBriefcase className="text-2xl" />
+              </div>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  <a
+                    href="#home"
+                    className="rounded-md bg-gray-950 px-3 py-2 text-sm font-medium text-white"
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="#aboutme"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                  >
+                    About
+                  </a>
+                  
+                  <a
+                    href="#skills"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                  >
+
+                    Skills
+                  </a>
+                  <a
+                    href="#education"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                  >
+                    Education
+                  </a>
+                  <a
+                    href="#projects"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                  >
+                    Projects
+                  </a>
+                  <a
+                    href="#contact"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-blue-950 hover:text-white"
+                  >
+                    Contact
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="relative ml-3">
+                <div>
+                  <button
+                    type="button"
+                    className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    id="user-menu-button"
+                    aria-expanded="false"
+                    aria-haspopup="true"
+                  >
+                    <span className="sr-only">Open user menu</span>
+                    <Image
+                      className="h-8 w-8 rounded-full border border-blue-200"
+                      src="/asma.jpg"
+                      alt="Dynamic Image"
+                      width="20"
+                      height="20"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile Menu */}
+        <div
+          className={`sm:hidden ${isMobileMenuOpen ? "block" : "hidden"}`}
+          id="mobile-menu"
+        >
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            <a
+              href="#home"
+              className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+              aria-current="page"
+            >
+              Home
+            </a>
+            <a
+              href="#aboutme"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              About
+            </a>
+            <a
+              href="#skills"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              Skills
+            </a>
+            <a
+              href="#education"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              Education
+            </a>
+            <a
+              href="#projects"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              Projects
+            </a>
+            <a
+              href="#contact"
+              className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              Contact
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div className="pt-16"></div>
+    </div>
+  );
+};
+
+export default Navbar;
